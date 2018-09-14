f42a9e9a29a7a6453dbdc3335a9ea3361161dc4e
charlyberthet <charlyberthet94@gmail.com>
2018-09-14 18:20:32 +0200

    ok

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/api/', async (req, res) => {
             console.log("headers", k, content.headers[k]);
             res.header(k, content.headers[k]);
         }*/
-        res.header("set-cookie","test");
+        res.header("test","test");
         res.send( content.data );
     } catch(e){
         res.status(500);
